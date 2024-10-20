b0f5b47603f455bb6baad3e94fbfc28bec85715d
Jari Kemppi <jari.kemppi@gmail.com>
2024-10-20 19:02:50 +0300

    Laajennettu arvauspeliä lisäämällä loputtomat kierrokset. Käyttäjä voi lopettaa arvailun kirjoittamalla loppu, jolloin ohjelma kertoo arvauksien määrän ja oikea nimen.

--- Arvauspeli.java
+++ Arvauspeli.java
@@ -4,17 +4,29 @@ public class Arvauspeli {
     public static void main(String[] args) {
         String oikeaNimi = "Matti";
         Scanner scanner = new Scanner(System.in);
+        int yritykset = 0;
+        String arvaus = "";
 
         System.out.println("Tervetuloa Arvauspeliin!");
-        System.out.print("Arvaa nimi: ");
-        String arvaus = scanner.nextLine();
+        System.out.println("Arvaa nimi tai kirjoita 'loppu' lopettaaksesi pelin.");
 
-        if (arvaus.equalsIgnoreCase(oikeaNimi)) {
-            System.out.println("Onnittelut! Arvasit oikein.");
-        } else {
-            System.out.println("Väärä arvaus. Peli päättyy.");
+        while (!arvaus.equalsIgnoreCase(oikeaNimi)) {
+            System.out.print("Arvaa nimi: ");
+            arvaus = scanner.nextLine();
+            yritykset++;
+
+            if (arvaus.equalsIgnoreCase(oikeaNimi)) {
+                System.out.println("Onnittelut! Arvasit oikein.");
+            } else if (arvaus.equalsIgnoreCase("loppu")) {
+                System.out.println("Peli lopetettu. Oikea nimi oli: " + oikeaNimi);
+                yritykset--;
+                break;
+            } else {
+                System.out.println("Väärä arvaus. Yritä uudelleen.");
+            }
         }
 
+        System.out.println("Arvauskertoja: " + yritykset);
         scanner.close();
     }
 }
